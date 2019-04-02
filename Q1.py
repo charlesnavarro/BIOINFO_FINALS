8bc2e5130649e1d6ffa233dde5b4ec5b77a3125d
charlesnavarro <37466960+charlesnavarro@users.noreply.github.com>
2019-04-02 20:27:49 +0800

    ????

--- Q1.py
+++ Q1.py
@@ -33,17 +33,19 @@ d2 = DiscreteDistribution({'A': 0.2700, 'C': 0.2084, 'G': 0.1981, 'T': 0.3236})
 s1 = State(d1, name="cg")
 s2 = State(d2, name="at")
 
-model = HiddenMarkovModel()
+model = HiddenMarkovModel(sequence)
 
 model.add_states(s1, s2)
 model.add_transition(model.start, s1, 1.0)
 model.add_transition(s1, s1, 0.9998)
-model.add_transition(s1, s2, 0.3)
+model.add_transition(s1, s2, 0.2)
 model.add_transition(s2, s2, 0.9998)
 model.add_transition(s2, s1, 0.2)
 model.add_transition(s2, model.end, 0.1)
 model.bake()
 
-print(model)
-model.viterbi(sequence)
+print(model.predict(sequence, algorithm='viterbi'))
+# example-start, s1, s2, s2, s2, s2, s2, s2, s2, s2, s2, s2, s2,
+# print(model)
+# model.viterbi(sequence)
 
