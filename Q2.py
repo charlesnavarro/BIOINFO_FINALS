eb31067df146d277c5fca75ae94d3b1a143db645
charlesnavarro <37466960+charlesnavarro@users.noreply.github.com>
2019-04-03 15:51:19 +0800

    no scoretable

--- Q2.py
+++ Q2.py
@@ -11,13 +11,13 @@ max = Si-1, j-1 + M[X[i],Y[j]]
 '''
 
 #tacggtat
-f = open("string1.txt", "r")
+f = open("1-AY278489.2.txt", "r")
 if f.mode == 'r':
-    x = (f.read())
+    x = (f.read()).lower()
 #ggacgtacg
-f = open("string2.txt", "r")
+f = open("2-AY394997.1.txt", "r")
 if f.mode == 'r':
-    y = (f.read())
+    y = (f.read()).lower()
 print(x, y)
 #gap = d
 
@@ -58,80 +58,80 @@ def getMax (m1, m2, m3):
             max = m3
     return max
 
-def getScore (i, j, g):
-    m1 = 0
-    m2 = 0
-    m3 = 0
-    max = 0
-    print(i-1, j-1)
-    m2 = scoreTable[i][j - 1] + g
-    m3 = scoreTable[i - 1][j] + g
-    #a compared to all
-    if (x[j-1] == 'a' and y[i-1] == 'a'):
-        m1 = scoreTable[i - 1][j - 1] + matrix[0][0]
-        print("aa")
-    elif (x[j-1] == 'a' and y[i-1] == 'c'):
-        m1 = scoreTable[i - 1][j - 1] + matrix[0][1]
-        print("ac")
-    elif (x[j-1] == 'a' and y[i-1] == 't'):
-        m1 = scoreTable[i - 1][j - 1] + matrix[0][2]
-        print("at")
-    elif (x[j-1] == 'a' and y[i-1] == 'g'):
-        m1 = scoreTable[i - 1][j - 1] + matrix[0][3]
-        print("ag")
-    #c paired to all
-    elif (x[j-1] == 'c' and y[i-1] == 'a'):
-        m1 = scoreTable[i - 1][j - 1] + matrix[1][0]
-        print("ca")
-    elif (x[j-1] == 'c' and y[i-1] == 'c'):
-        m1 = scoreTable[i - 1][j - 1] + matrix[1][1]
-        print("cc")
-    elif (x[j-1] == 'c' and y[i-1] == 't'):
-        m1 = scoreTable[i - 1][j - 1] + matrix[1][2]
-        print("ct")
-    elif (x[j-1] == 'c' and y[i-1] == 'g'):
-        m1 = scoreTable[i - 1][j - 1] + matrix[1][3]
-        print("cg")
-    #t compared to all
-    elif (x[j-1] == 't' and y[i-1] == 'a'):
-        m1 = scoreTable[i - 1][j - 1] + matrix[2][0]
-        print("ta")
-    elif (x[j-1] == 't' and y[i-1] == 'c'):
-        m1 = scoreTable[i - 1][j - 1] + matrix[2][1]
-        print("tc")
-    elif (x[j-1] == 't' and y[i-1] == 't'):
-        m1 = scoreTable[i - 1][j - 1] + matrix[2][2]
-        print("tt")
-    elif (x[j-1] == 't' and y[i-1] == 'g'):
-        print("tg")
-        m1 = scoreTable[i - 1][j - 1] + matrix[2][3]
-    #g compared to all
-    elif (x[j-1] == 'g' and y[i-1] == 'a'):
-        m1 = scoreTable[i - 1][j - 1] + matrix[3][0]
-        print("ga")
-    elif (x[j-1] == 'g' and y[i-1] == 'c'):
-        m1 = scoreTable[i - 1][j - 1] + matrix[3][1]
-        print("gc")
-    elif (x[j-1] == 'g' and y[i-1] == 't'):
-        m1 = scoreTable[i - 1][j - 1] + matrix[3][2]
-        print("gt")
-    elif (x[j-1] == 'g' and y[i-1] == 'g'):
-        m1 = scoreTable[i - 1][j - 1] + matrix[3][3]
-        print("gg")
-
-    max = getMax(m1, m2, m3)
-
-    # if ((i == 3 and j == 4) or (i == 9 and j == 6)):
-    #     print(m2, m3, max)
-    #     print("HERE")
-    print(i, j, g)
-    print(m1, m2, m3, max)
-    return max
+# def getScore (i, j, g):
+#     m1 = 0
+#     m2 = 0
+#     m3 = 0
+#     max = 0
+#     print(i-1, j-1)
+#     m2 = scoreTable[i][j - 1] + g
+#     m3 = scoreTable[i - 1][j] + g
+#     #a compared to all
+#     if (x[j-1] == 'a' and y[i-1] == 'a'):
+#         m1 = scoreTable[i - 1][j - 1] + matrix[0][0]
+#         print("aa")
+#     elif (x[j-1] == 'a' and y[i-1] == 'c'):
+#         m1 = scoreTable[i - 1][j - 1] + matrix[0][1]
+#         print("ac")
+#     elif (x[j-1] == 'a' and y[i-1] == 't'):
+#         m1 = scoreTable[i - 1][j - 1] + matrix[0][2]
+#         print("at")
+#     elif (x[j-1] == 'a' and y[i-1] == 'g'):
+#         m1 = scoreTable[i - 1][j - 1] + matrix[0][3]
+#         print("ag")
+#     #c paired to all
+#     elif (x[j-1] == 'c' and y[i-1] == 'a'):
+#         m1 = scoreTable[i - 1][j - 1] + matrix[1][0]
+#         print("ca")
+#     elif (x[j-1] == 'c' and y[i-1] == 'c'):
+#         m1 = scoreTable[i - 1][j - 1] + matrix[1][1]
+#         print("cc")
+#     elif (x[j-1] == 'c' and y[i-1] == 't'):
+#         m1 = scoreTable[i - 1][j - 1] + matrix[1][2]
+#         print("ct")
+#     elif (x[j-1] == 'c' and y[i-1] == 'g'):
+#         m1 = scoreTable[i - 1][j - 1] + matrix[1][3]
+#         print("cg")
+#     #t compared to all
+#     elif (x[j-1] == 't' and y[i-1] == 'a'):
+#         m1 = scoreTable[i - 1][j - 1] + matrix[2][0]
+#         print("ta")
+#     elif (x[j-1] == 't' and y[i-1] == 'c'):
+#         m1 = scoreTable[i - 1][j - 1] + matrix[2][1]
+#         print("tc")
+#     elif (x[j-1] == 't' and y[i-1] == 't'):
+#         m1 = scoreTable[i - 1][j - 1] + matrix[2][2]
+#         print("tt")
+#     elif (x[j-1] == 't' and y[i-1] == 'g'):
+#         print("tg")
+#         m1 = scoreTable[i - 1][j - 1] + matrix[2][3]
+#     #g compared to all
+#     elif (x[j-1] == 'g' and y[i-1] == 'a'):
+#         m1 = scoreTable[i - 1][j - 1] + matrix[3][0]
+#         print("ga")
+#     elif (x[j-1] == 'g' and y[i-1] == 'c'):
+#         m1 = scoreTable[i - 1][j - 1] + matrix[3][1]
+#         print("gc")
+#     elif (x[j-1] == 'g' and y[i-1] == 't'):
+#         m1 = scoreTable[i - 1][j - 1] + matrix[3][2]
+#         print("gt")
+#     elif (x[j-1] == 'g' and y[i-1] == 'g'):
+#         m1 = scoreTable[i - 1][j - 1] + matrix[3][3]
+#         print("gg")
+#
+#     max = getMax(m1, m2, m3)
+#
+#     # if ((i == 3 and j == 4) or (i == 9 and j == 6)):
+#     #     print(m2, m3, max)
+#     #     print("HERE")
+#     print(i, j, g)
+#     print(m1, m2, m3, max)
+#     return max
 
 score = 0
 total = 0
 miss = 0
-scoreTable = numpy.zeros(shape=(len(y)+1, len(x)+1), dtype=int)
+# scoreTable = numpy.zeros(shape=(len(y)+1, len(x)+1), dtype=int)
 #            r  c
 # scoreTable[0][1] = 1
 
@@ -147,8 +147,8 @@ for i in range(0, len(y)+1):
     for j in range(0, len(x)+1):
         #NOT A BASE CASE
         if(i != 0 and j != 0):
-            score = getScore(i, j, g)
-            scoreTable[i][j] = score
+            # score = getScore(i, j, g)
+            # scoreTable[i][j] = score
             if(x[j-1] == "a" and y[i-1] == "c"):
                 miss = miss + 1
                 total = total + 1
@@ -194,13 +194,14 @@ for i in range(0, len(y)+1):
             if (x[j-1] == "g" and y[i-1] == "g"):
                 total = total + 1
         #BASE CASE
-        if(i == 0):
-            scoreTable[i][j] = j * g
+        # if(i == 0):
+            #scoreTable[i][j] = j * g
         #BASE CASE
-        elif(j == 0):
-            scoreTable[i][j] = i * g
-        print(scoreTable, "\n")
+        # elif(j == 0):
+            #scoreTable[i][j] = i * g
+        # print(scoreTable, "\n")
 fraction = miss / total
+print("hi")
 print(miss)
 print(total)
 print(fraction)
