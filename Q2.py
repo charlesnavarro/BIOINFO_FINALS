de6befb7fcea7dca19ff1ba40bddab80b6f70213
brentlim98 <brent_lim@dlsu.edu.ph>
2019-04-09 18:46:49 +0800

    edited variables

--- Q2.py
+++ Q2.py
@@ -10,23 +10,23 @@ def readSequence(filename): #read sequence function
 def get_distance(alignment1, alignment2, score, begin, end): #distance function
     match = 0
     mismatch = 0
-    s = []
-    s.append("%s\n" % alignment1)
-    s.append(" " * begin)
+    values = []
+    values.append("%s\n" % alignment1)
+    values.append(" " * begin)
     for a, b in zip(alignment1[begin:end], alignment2[begin:end]):
         if a == b:
-            s.append("|")
+            values.append("|")
             match = match + 1
         elif a == "-" or b == "-":
-            s.append(" ")
+            values.append(" ")
         else:
-            s.append(".")
+            values.append(".")
             mismatch = mismatch + 1
-    s.append("\n")
-    s.append("%s\n" % alignment2)
-    s.append("score=%f\n" % score)
-    s.append("match=%d\n" % match)
-    s.append("mismatch=%d\n" % mismatch)
+    values.append("\n")
+    values.append("%s\n" % alignment2)
+    values.append("score=%f\n" % score)
+    values.append("match=%d\n" % match)
+    values.append("mismatch=%d\n" % mismatch)
     distance = mismatch / (match + mismatch)
     return distance
 
@@ -36,7 +36,7 @@ constructor = DistanceTreeConstructor()
 virus = [readSequence("1-AY278489.2.fasta"), readSequence("2-AY394997.1.fasta"), readSequence("3-AY394978.1.fasta"), readSequence("4-AY278554.2.fasta"), readSequence("5-AY278741.1.fasta"), readSequence("6-AY274119.3.fasta"),readSequence("7-AY283794.1.fasta"), readSequence("8-AY291451.1.fasta"), readSequence("9-AY345986.1.fasta"), readSequence("10-AY394999.1.fasta"), readSequence("11-AY572034.1.fasta")]
 names = ["Guangzhou (Dec 2002)", "Zhongshan", "Guangzhou (Jan 2003)", "Hong Kong (Feb 2003)", "Hanoi", "Toronto", "Singapore", "Taiwan", "Hong Kong (Mar 2003)", "Hong Kong (May 2003)", "Palm Civet"]
 
-distanceMatrix = []
+dMatrix = []
 
 for x in range(0, 11):
     mList = []
@@ -51,8 +51,8 @@ for x in range(0, 11):
                 if max_mismatch < curr_mismatch:
                     max_mismatch = curr_mismatch
             mList.append(max_mismatch)
-    distanceMatrix.append(mList)
+    dMatrix.append(mList)
 
-dMatrix = DistanceMatrix(names, distanceMatrix)
-upgmaTree = constructor.upgma(dMatrix)
+distanceMatrix = DistanceMatrix(names, dMatrix)
+upgmaTree = constructor.upgma(distanceMatrix)
 print(upgmaTree)
