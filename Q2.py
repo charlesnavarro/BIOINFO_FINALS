495c190301c5183f70b1fd6abe664f41b7891cbb
brentlim98 <brent_lim@dlsu.edu.ph>
2019-04-09 18:03:24 +0800

    fix

--- Q2.py
+++ Q2.py
@@ -1,69 +1,58 @@
-# Import libraries
 from Bio import pairwise2
 from Bio import SeqIO
 from Bio.Phylo.TreeConstruction import DistanceMatrix, DistanceTreeConstructor
 
-# Define function readfile
-def readSequence(filename):
+def readSequence(filename): #read sequence function
     file = open(filename)
     sequence = SeqIO.read(file, "fasta")
     return sequence
 
-def get_distance(alignment1, alignment2, score, begin, end):
-    mismatch = 0
+def get_distance(alignment1, alignment2, score, begin, end): #distance function
     match = 0
-    gap = 0
+    mismatch = 0
     s = []
     s.append("%s\n" % alignment1)
     s.append(" " * begin)
     for a, b in zip(alignment1[begin:end], alignment2[begin:end]):
         if a == b:
-            s.append("|")  # match
+            s.append("|")
             match = match + 1
         elif a == "-" or b == "-":
-            s.append(" ")  # gap
-            gap = gap + 11
+            s.append(" ")
         else:
-            s.append(".")  # mismatch
+            s.append(".")
             mismatch = mismatch + 1
     s.append("\n")
     s.append("%s\n" % alignment2)
-    s.append("score=%g\n" % score)
-    s.append("match=%g\n" % match)
-    s.append("gap=%g\n" % gap)
-    s.append("mismatch=%g\n" % mismatch)
-    distance = mismatch / match
+    s.append("score=%f\n" % score)
+    s.append("match=%d\n" % match)
+    s.append("mismatch=%d\n" % mismatch)
+    distance = mismatch / (match + mismatch)
     return distance
 
 
 constructor = DistanceTreeConstructor()
 
-sequences = [readSequence("1-AY278489.2.fasta"), readSequence("2-AY394997.1.fasta"), readSequence("3-AY394978.1.fasta"),
-             readSequence("4-AY278554.2.fasta"), readSequence("5-AY278741.1.fasta"), readSequence("6-AY274119.3.fasta"),
-             readSequence("7-AY283794.1.fasta"), readSequence("8-AY291451.1.fasta"), readSequence("9-AY345986.1.fasta"),
-             readSequence("10-AY394999.1.fasta"), readSequence("11-AY572034.1.fasta")]
-
-names = ["Guangzhou", "Zhongshan", "Guangzhou2", "Hong Kong", "Hanoi", "Toronto", "Singapore", "Taiwan", "Hong Kong 2", "Hong Kong 3", "Palm Civet"]
+virus = [readSequence("1-AY278489.2.fasta"), readSequence("2-AY394997.1.fasta"), readSequence("3-AY394978.1.fasta"), readSequence("4-AY278554.2.fasta"), readSequence("5-AY278741.1.fasta"), readSequence("6-AY274119.3.fasta"),readSequence("7-AY283794.1.fasta"), readSequence("8-AY291451.1.fasta"), readSequence("9-AY345986.1.fasta"), readSequence("10-AY394999.1.fasta"), readSequence("11-AY572034.1.fasta")]
+names = ["Guangzhou (Dec 2002)", "Zhongshan", "Guangzhou (Jan 2003)", "Hong Kong (Feb 2003)", "Hanoi", "Toronto", "Singapore", "Taiwan", "Hong Kong (Mar 2003)", "Hong Kong (May 2003)", "Palm Civet"]
 
 distanceMatrix = []
 
 for x in range(0, 11):
-    oneList = []
+    mList = []
     for y in range(0, 11):
         if x == y:
-            oneList.append(0)
+            mList.append(0)
         elif x > y:
-            alignments = pairwise2.align.globalxx(sequences[x], sequences[y])
+            alignments = pairwise2.align.globalxx(virus[x], virus[y])
             max_mismatch = 0
-            for aln in alignments:
-                curr_mismatch = get_distance(*aln)
+            for z in alignments:
+                curr_mismatch = get_distance(*z)
                 if max_mismatch < curr_mismatch:
                     max_mismatch = curr_mismatch
-            oneList.append(max_mismatch)
-    distanceMatrix.append(oneList)
+            mList.append(max_mismatch)
+    distanceMatrix.append(mList)
 
 dMatrix = DistanceMatrix(names, distanceMatrix)
 upgmaTree = constructor.upgma(dMatrix)
 print(upgmaTree)
-
-
